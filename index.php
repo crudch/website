c721219ca4cbb02b8f3cfb8910acfbec8dc7bae1
rovogit <tequi11az@mail.ru>
2019-03-19 18:57:56 +0500

    CRUDch ver. 0.1

--- index.php
+++ index.php
@@ -4,27 +4,40 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="theme-color" content="#fff">
-    <title>ЕКАМЕТ</title>
+    <title>CRUDch</title>
     <link rel="stylesheet" type="text/css" href="/css/normalize.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
 <body>
 <nav class="navigation">
-    <ul id="navigation-active" class="navigation__list navigation__list--hide">
-        <li><a href="#!" data-scroll="prices">Цены</a></li>
+    <a class="navigation__logo" href="/">
+        <img src="/img/logo.svg" alt="CRUDch logo">
+    </a>
+    <ul class="navigation__list">
+        <li><a href="#">service</a></li>
+        <li><a href="#">catalogue</a></li>
+        <li><a href="#">prices</a></li>
+        <li><a href="#">contacts</a></li>
+    </ul>
+    <ul class="navigation__phone">
+        <li>
+            <a href="tel:+79507777777">+7 (950) 777 77 77</a>
+        </li>
     </ul>
 </nav>
 <header class="header">
-    <h1></h1>
+    <h1>CRUDch INC.</h1>
+    <h2>Делаем под крудч</h2>
+    <h3>Быстро и крачественно</h3>
 </header>
 <main class="main">
-    <section id="primary" class="primary">
-            <article class="primary__advantages">
+    <section>
+            <article>
             </article>
     </section>
 </main>
 <footer class="footer">
-    <p><?php echo date('Y'); ?></p>
+    <p><?php echo date('Y'); ?> © CRUDch incorporated. All rights reserved.</p>
 </footer>
 <script>
 </script>
